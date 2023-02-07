247ac82009edc4409d690d0d9d1d17c1884e318c
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-02-07 12:34:22 +0200

    add proekt

---
+++ rpp/rpp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace rpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(nastroiki.CountLess100(arr));
+            Console.WriteLine(nastroiki.MaxNum(arr));
+            arr = nastroiki.ReverseArr(arr);
+            nastroiki.PrintArr(arr);
+
+        }
+    }
+}
