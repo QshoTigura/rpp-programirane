ecee33b9dfe63fcc479d1c5bd20ab088f1ad98ea
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-02-07 13:27:32 +0200

    ne e gotova

---
+++ rpp/masivi2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace masivi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
