0680ccc250e8c8fb9ef8491532a1112c00aaf247
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-02-21 12:13:00 +0200

    math.sqrt

---
+++ chislaKvadrati/chislaKvadrati/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace chislaKvadrati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var result = nums.Where(x => Math.Sqrt(x) == (int)Math.Sqrt(x)).ToList();
+            Console.WriteLine(string.Join(" ",result));
+            
+        }
+    }
+}
